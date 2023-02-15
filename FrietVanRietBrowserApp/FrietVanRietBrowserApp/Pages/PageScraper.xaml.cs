e4052e299a0fe0ec5c8fcbc93ed2bede5ff5b031
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-02-15 14:42:56 +0100

    trying to scrape

--- FrietVanRietBrowserApp/FrietVanRietBrowserApp/Pages/PageScraper.xaml.cs
+++ FrietVanRietBrowserApp/FrietVanRietBrowserApp/Pages/PageScraper.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CsvHelper;
+using HtmlAgilityPack;
 
 namespace FrietVanRietBrowserApp.Pages
 {
@@ -20,9 +26,52 @@ namespace FrietVanRietBrowserApp.Pages
     /// </summary>
     public partial class PageScraper : Page
     {
+        public class Row
+        {
+            public string Title { get; set; }
+        }
+
         public PageScraper()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void btnGetScraper_Click(object sender, RoutedEventArgs e)
+        {
+  //          var html =
+  //      @"<TD class=texte width=""50%"">
+		//	<DIV align=right>Name :<B> </B></DIV>
+		//</TD>
+		//<TD width=""50%"">
+  //  		<INPUT class=box value=John itemscope='test' maxLength=16 size=16 name=user_name>
+  //  		<INPUT class=box value=Tony itemscope='test' maxLength=16 size=16 name=user_name>
+  //  		<INPUT class=box value=Jams itemscope='test' maxLength=16 size=16 name=user_name>
+  //          <p itemscope='hallo'>Goedendag</p>
+		//</TD>
+		//<TR vAlign=center>";
+
+  //          var htmlDoc = new HtmlDocument();
+  //          htmlDoc.LoadHtml(html);
+
+            string url = "https://frietvanriet.foodticket.nl/foodticket/cgi/bestel.cgi#menu";
+            HtmlWeb web = new HtmlWeb();
+            var myHtmlDoc = web.Load(url);
+
+            var getArticle = myHtmlDoc.DocumentNode.SelectNodes("//*[@itemtype=\"http://schema.org/Product\"]");
+
+
+
+            //string myName = htmlDoc.Text;
+
+
+            //string name = htmlDoc.DocumentNode
+            //                .SelectNodes("//td/p")
+            //                .First()
+            //                .Attributes["itemscope"].Value;
+
+            //Console.WriteLine(name);
         }
     }
 }
